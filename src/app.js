fc237be87703edca83e3eb21488fd1d3cd6538ef
polychrom <poly.chrom@outlook.com>
2022-04-02 11:44:50 +0200

    added basic functions and layout

--- src/app.js
+++ src/app.js
@@ -10,23 +10,30 @@ const homeUrl = window.location.origin + window.location.pathname;
 
 const items = 20;
 const modal = document.getElementById("modal");
-const modal_id = document.getElementById("modal_id");
 const project_count = document.getElementById("project_count");
 
 var range = document.getElementById("range");
 
 const gallery = document.getElementById("gallery");
 
-function setGridItems(items, save = true) {
+function setGridItems(items, elemx, save = true) {
+  btnActiveGrid = document.querySelectorAll(".btn-active-grid");
+  console.log("button", btnActiveGrid);
+
+  [].forEach.call(btnActiveGrid, function (el) {
+    el.classList.remove("btn-active-grid");
+  });
+
+  if (elemx) {
+    elemx.classList.add("btn-active-grid");
+  }
+
   let menge = "";
   const amount = "1fr ";
   for (i = 0; i < items; i++) {
     menge += amount;
   }
   gallery.style.gridTemplateColumns = menge;
-
-  const btn = document.getElementById("grid_2");
-  btn.style.borderColor = "#ff0000";
 
   if (save) {
     localStorage.setItem("grid", items);
@@ -35,11 +42,14 @@ function setGridItems(items, save = true) {
 
 function createGalleryItem(id, data) {
   const project_dir = data.project[id].project_dir;
-  console.log("log", project_dir);
+  const project_name = project_dir;
+
+  const project_year = data.project[id].year;
 
   const link = document.createElement("a");
-  var linkText = document.createTextNode("" + data.project[id].title + id);
-  link.href = "#" + id;
+  link.href = "#" + project_dir;
+  link.setAttribute("data-year", project_year);
+  link.setAttribute("data-name", project_name);
 
   gallery.appendChild(link);
 
@@ -49,8 +59,11 @@ function createGalleryItem(id, data) {
   galleryItemInfo = document.createElement("div");
   galleryItemInfo.setAttribute("class", "gallery__item__info");
 
-  // const galleryItemTags = document.createElement("span");
-  //  galleryItemTags.setAttribute("class", "gallery__item__tags");
+  const galleryItemIcons = document.createElement("div");
+  galleryItemIcons.setAttribute(
+    "class",
+    "gallery__item__icons icon icon-video"
+  );
 
   const x = document.createTextNode(data.project[id].tools);
 
@@ -58,11 +71,11 @@ function createGalleryItem(id, data) {
   link.appendChild(galleryItem);
 
   galleryItem.appendChild(galleryItemInfo);
+  galleryItem.appendChild(galleryItemIcons);
+
   galleryItemInfo.appendChild(x);
 
   galleryItemInfo.innerHTML = data.project[id].title;
-
-  //console.log(galleryItemTags);
 
   const currentGalleryItem = document.querySelectorAll(".gallery__item")[id];
   currentGalleryItem.style.backgroundImage = `url( ${
@@ -81,24 +94,99 @@ function mountGallery(data) {
   for (id = 0; id <= items; id++) {
     createGalleryItem(id, data);
   }
-  setGridItems(localStorage.getItem("grid"));
+  // setGridItems(localStorage.getItem("grid"));
+}
+
+function filter(year = 0, activeElement) {
+  const timeline = document.getElementById("timeline");
+  console.log("timeline", timeline);
+  timeline.innerHTML = year == 0 ? "All" : year;
+
+  x = document.querySelectorAll(".btn-active");
+
+  [].forEach.call(x, function (el) {
+    el.classList.remove("btn-active");
+  });
+
+  if (activeElement) {
+    activeElement.classList.add("btn-active");
+  }
+
+  console.log(activeElement);
+
+  const items = document.getElementsByTagName("a");
+  year = year.toString();
+  let toRemove = 0;
+
+  for (item of items) {
+    item.classList.remove("gallery__item__hide");
+  }
+
+  for (item of items) {
+    if (item.dataset.year != year && year != 0) {
+      toRemove += 1;
+      item.setAttribute("class", "gallery__item__hide");
+    }
+  }
+  updateProjectCount(items.length - toRemove);
 }
 
 function openModal(data) {
-  const currentId = getIdFromUrl();
-  const currentProject = data.project[currentId];
-  console.log("id", currentId);
-  modal.style.visibility = "visible";
-  modal_id.innerHTML = currentId + currentProject.title;
+  const mod = document.getElementById("modal_id");
+  if (mod != null) {
+    mod.remove();
+  }
 
-  const img = document.createElement("img");
+  const currentId = getIdFromHash(data);
+  const currentProject = data.project[getIdFromHash(data)];
+
+  console.log("id", currentId);
+  const modal_id = document.createElement("div");
+  modal_id.setAttribute("id", "modal_id");
+
+  modal.appendChild(modal_id);
+
+  const div = document.createElement("div");
+  div.setAttribute("class", "modal__wrapper");
+  modal_id.appendChild(div);
+
+  const textbox = document.createElement("div");
+  textbox.innerHTML = "fweverververv";
+  textbox.setAttribute("class", "textbox");
+  div.appendChild(textbox);
+
+  li_project_name = document.createElement("li");
+  li_project_name.innerHTML = currentProject.project_dir.toUpperCase();
+  textbox.appendChild(li_project_name);
+
+  li_year = document.createElement("li");
+  li_year.innerHTML = currentProject.year;
+  textbox.appendChild(li_year);
+
+  modal.style.visibility = "visible";
+  /*
+  div.innerHTML =
+    currentId +
+    " / " +
+    currentProject.project_dir +
+    " / " +
+    currentProject.title +
+    " / " +
+    currentProject.year;
+    */
+
   const imageSet = currentProject.images.length;
 
-  console.log(imageSet);
   for (i = 0; i < imageSet; i++) {
-    img.src = root_dir + data.project[currentId].images[i];
+    const img = document.createElement("img");
 
-    modal_id.appendChild(img);
+    img.src =
+      root_dir +
+      currentProject.project_dir +
+      "/" +
+      data.project[getIdFromHash(data)].images[i];
+
+    div.appendChild(img);
   }
 }
 
@@ -106,7 +194,7 @@ function updateProjectCount(count) {
   let count_start = 0;
   setInterval(function () {
     if (count_start < count) {
-      project_count.innerHTML = count_start += 1;
+      //project_count.innerHTML = count_start += 1;
     }
   }, 100);
 }
@@ -116,8 +204,16 @@ function closeModal() {
   window.history.pushState(null, null, homeUrl);
 }
 
-function getIdFromUrl() {
-  return window.location.href.split("#")[1];
+function getIdFromHash(data) {
+  const allProjects = Object.keys(data.project).length;
+  const hash = window.location.href.split("#")[1];
+  console.log("hash", hash);
+
+  for (id = 0; id < allProjects; id++) {
+    if (data.project[id].project_dir == hash) {
+      return id;
+    }
+  }
 }
 
 function init() {
